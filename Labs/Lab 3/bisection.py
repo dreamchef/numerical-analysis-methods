e0ad3eed3213b8a83a756df808112b6661424f67
SlowHands7 <danilisle74@gmail.com>
2023-02-10 23:55:43 -0700

    bisection code and function

---
+++ Labs/Lab 3/bisection.py
@@ -0,0 +1,85 @@
+import sys
+sys.path.append('../..')
+import mypkg.prini as prini
+def driver():
+
+    f = lambda x: x**2 * (x-1)
+    a = -1    # 0.5  -1    -1
+    b =  2    # 2     0.5   2
+
+    Nmax = 100
+    tol = 1e-6
+
+    [astar,ier] = bisection(f,a,b,tol,Nmax)
+    tmp = prini('real','the approximate root is',astar)
+    tmp.print()
+    tmp = prini('inter','the error message reads:',ier)
+    tmp.print()
+
+def bisection(f,a,b,tol,Nmax):
+    '''
+    Inputs:
+      f,a,b       - function and endpoints of initial interval
+      tol, Nmax   - bisection stops when interval length < tol
+                  - or if Nmax iterations have occured
+    Returns:
+      astar - approximation of root
+      ier   - error message
+            - ier = 1 => cannot tell if there is a root in the interval
+            - ier = 0 == success
+            - ier = 2 => ran out of iterations
+            - ier = 3 => other error ==== You can explain
+    '''
+
+    '''     first verify there is a root we can find in the interval '''
+    fa = f(a); fb = f(b);
+    if (fa*fb>0):
+       ier = 1
+       astar = a
+       return [astar, ier]
+
+    ''' verify end point is not a root '''
+    if (fa == 0):
+      astar = a
+      ier =0
+      return [astar, ier]
+
+    if (fb ==0):
+      astar = b
+      ier = 0
+      return [astar, ier]
+
+    count = 0
+    while (count < Nmax):
+      c = 0.5*(a+b)
+      fc = f(c)
+
+      if (fc ==0):
+        astar = c
+        ier = 0
+        return [astar, ier]
+
+      if (fa*fc<0):
+         b = c
+      elif (fb*fc<0):
+        a = c
+        fa = fc
+      else:
+        astar = c
+        ier = 3
+        return [astar, ier]
+
+      if (abs(b-a)<tol):
+        astar = a
+        ier =0
+        return [astar, ier]
+      
+      count = count +1
+
+    astar = a
+    ier = 2
+    return [astar,ier] 
+
+if __name__ == '__main__':
+    # run the drivers only if this is called from the command line
+    driver()
