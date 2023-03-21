a6b25f40bf9be3820f0ed7b91d41f7df3e92b2d7
Dani <121541458+slowHands7@users.noreply.github.com>
2023-03-21 09:04:44 -0600

    Prelab 8.2 Work

--- Labs/Lab 8/prelab8.py
+++ Labs/Lab 8/prelab8.py
@@ -2,7 +2,7 @@ import math as m
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Returns 2D vector of points (inner) in each interval (outer).
+# Return 2D vector of points (inner) in each interval (outer).
 def pointsInIntervals (xeval, xint):
     
     pointsInIntervals = []
@@ -25,12 +25,32 @@ def pointsInIntervals (xeval, xint):
     
     return pointsInIntervals
     
+# Evaluates line given value in the domain.
+def evaluateLine(x0, x1, y0,y1, val):
+
+    slope = (y1 - y0) / (x1 - x0)
+    
+    yIntercept = y0 - slope * x0
+
+    line = lambda x: slope * x + yIntercept
+
+
+    # Evaluate.
+    y = line(val)
+    
+    
+    return y
+    
+
     
 # Test
 
 xeval = np.linspace(0,10,1000)
 
 xint = np.linspace(0,10,11)
+
+
+print(evaluateLine(0,1,0,1,2))
 
 
 # print(pointsInIntervals(xeval, xint)[7])
