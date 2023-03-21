bd69adc6e68336175d3ae44f564166520e538ad8
Dani <121541458+slowHands7@users.noreply.github.com>
2023-03-21 09:36:59 -0600

    Lab 8 section 1

--- Labs/Lab 8/prelab8.py
+++ Labs/Lab 8/prelab8.py
@@ -3,37 +3,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Return 2D vector of points (inner) in each interval (outer).
-def pointsInIntervals (xeval, xint):
+def find_int (xeval, a, b):
     
-    pointsInIntervals = []
-    
-    for i in range(len(xint)-1):
+    pointsInInterval = []
         
-        pointsInSingleInterval = []
-        
-        
-        for point in xeval:
+    for i in range(len(xeval)):
             
-            
-            if xint[i] <= point and point <= xint[i+1]:
+        if a <= xeval[i] and xeval[i] <= b:
                 
-                pointsInSingleInterval.append(point)
-        
-        
-        pointsInIntervals.append(pointsInSingleInterval)
+            pointsInInterval.append(i)
     
-    
-    return pointsInIntervals
+    return pointsInInterval
     
 # Evaluates line given value in the domain.
-def evaluateLine(x0, x1, y0,y1, val):
+def evaluateLine(val, x0, y0, x1, y1):
 
     slope = (y1 - y0) / (x1 - x0)
     
     yIntercept = y0 - slope * x0
 
     line = lambda x: slope * x + yIntercept
-
 
     # Evaluate.
     y = line(val)
@@ -50,7 +39,5 @@ xeval = np.linspace(0,10,1000)
 xint = np.linspace(0,10,11)
 
 
-print(evaluateLine(0,1,0,1,2))
-
-
-# print(pointsInIntervals(xeval, xint)[7])
+#print(evaluateLine(0,1,0,1,2))
+print(find_int(xeval,1,2))
